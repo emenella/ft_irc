2df17105a86c0a46d33804212b3c597b64d4f1af
Emil Bellon <ebellon@z4r7p3.42lyon.fr>
2022-09-13 11:37:27 +0200

    ptite opti

--- srcs/client/command/PART.cpp
+++ srcs/client/command/PART.cpp
@@ -6,7 +6,7 @@
 /*   By: ebellon <ebellon@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 16:27:51 by emenella          #+#    #+#             */
-/*   Updated: 2022/09/08 17:59:17 by ebellon          ###   ########lyon.fr   */
+/*   Updated: 2022/09/13 11:31:28 by ebellon          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,8 @@ int PART::execute(Client &clicli, args_t::iterator begin, args_t::iterator end)
 {
     int ret = AuthenticationCommand::execute(clicli, begin, end);
     if (ret == 1)
-	{
-		begin++;
-		while (begin != end)
-        {
+		while (begin++ != end)
 			_serv->partChannel(*begin, clicli);
-			begin++;
-		}
-	}
 	else
         clicli << ERR_NOTREGISTERED;
     return 0;
